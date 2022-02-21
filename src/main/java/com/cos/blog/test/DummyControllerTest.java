1508783ebb564d437a81e46b51bf0aa05cd3e17f
changsup <changsup96@gmail.com>
2022-02-21 23:19:42 +0900

    Test update operation.

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -3,6 +3,8 @@ package com.cos.blog.test;
 import java.util.List;
 import java.util.function.Supplier;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,6 +13,8 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.blog.model.RoleType;
@@ -21,8 +25,32 @@ import com.cos.blog.repository.UserRepository;
 @RestController
 public class DummyControllerTest {
 	
-	@Autowired // 의존성 주입(DI)
+	@Autowired // 의존성 주입(DI) 
 	private UserRepository userRepository;
+	
+	
+	// save 함수는 ID를 전달 X -> INSERT
+	// save 함수는 ID를 전달 O -> 데이터 O -> UPDATE
+	// save 함수는 ID를 전달 O -> 데이터 X -> INSERT
+	// email, password
+	@Transactional
+	@PutMapping("/dummy/user/{id}")
+	public User updateUser(@PathVariable int id, @RequestBody User requestUser) { // JSON 데이터 요청: Java Object(MessageConverter의 Jackson 라이브러리가 변환)
+		System.out.println("id:" + id);
+		System.out.println("password:" + requestUser.getPassword());
+		System.out.println("email:" + requestUser.getEmail());
+		
+		User user = userRepository.findById(id).orElseThrow(() -> {
+			return new IllegalArgumentException("수정 실패");
+		});
+		
+		user.setPassword(requestUser.getPassword());
+		user.setEmail(requestUser.getEmail());
+		//userRepository.save(user);
+		
+		// 더티 체킹
+		return null;
+	}
 	
 	// http://localhost:8000/blog/dummy/user	
 	@GetMapping("/dummy/users")
@@ -34,7 +62,7 @@ public class DummyControllerTest {
 	@GetMapping("/dummy/user")
 	public List<User> pageList(@PageableDefault(size = 2, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
 		Page<User> pagingUser = userRepository.findAll(pageable);
-			
+		 	
 		List<User> users = pagingUser.getContent();
 		
 		return users;
