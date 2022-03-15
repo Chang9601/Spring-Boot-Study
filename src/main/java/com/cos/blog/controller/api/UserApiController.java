72f1f2136fa1e3bd47a6285c9fd3783e46b23711
changsup <changsup96@gmail.com>
2022-03-16 08:32:18 +0900

    Work on Spring Security login.

--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -1,7 +1,5 @@
 package com.cos.blog.controller.api;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,9 +17,6 @@ public class UserApiController {
 	@Autowired
 	private UserService userService;
 	
-	@Autowired
-	private HttpSession session;
-	
 	@PostMapping("/api/user")
 	public ResponseDto<Integer> save(@RequestBody User user) {
 		System.out.println("UserApiController: save 호출");
@@ -33,15 +28,15 @@ public class UserApiController {
 	}
 	
 	// 스프링 시큐리티 로그인
-	@PostMapping("/api/user/login")
-	public ResponseDto<Integer> login(@RequestBody User user) {
-		System.out.println("UserApiController: login 호출");
-		User principal = userService.로그인(user); // principal: 접근주체
-		
-		if(principal != null) {
-			session.setAttribute("principal", principal);
-		}
-		
-		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1); // 자바 객체를 JSON으로 변환해서 반환(Jackson)
-	}
+	/*
+	 * @PostMapping("/api/user/login") public ResponseDto<Integer>
+	 * login(@RequestBody User user, HttpSession session) {
+	 * System.out.println("UserApiController: login 호출"); User principal =
+	 * userService.로그인(user); // principal: 접근주체
+	 * 
+	 * if(principal != null) { session.setAttribute("principal", principal); }
+	 * 
+	 * return new ResponseDto<Integer>(HttpStatus.OK.value(), 1); // 자바 객체를 JSON으로
+	 * 변환해서 반환(Jackson) }
+	 */
 }
--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -21,14 +21,14 @@ let index = {
 		// ajax 통신 성공 후 서버가 JSON을 반환하면 자동으로 자바스크립트 객체로 변환
 		$.ajax({
 			type: "POST",
-			url: "/blog/api/user",
+			url: "/api/user",
 			contentType: "application/json; charset=UTF-8", // 요청 데이터의 MIME 타입
 			data: JSON.stringify(data), // 자바스크립트 객체 -> JSON 문자열, HTTP body
 			dataType: "json" // 응답 데이터의 타입, 기본적으로 모든 것은 문자열, JSON이면 자바스크립트 객체로 변경
 		}).done(function(resp) { // 응답 결과
 			alert("회원가입 완료");
 			//console.log(resp);
-			location.href = "/blog";
+			location.href = "/";
 		}).fail(function(error) {
 			alert(JSON.stringify(error));
 		});
@@ -42,13 +42,13 @@ let index = {
 
 		$.ajax({
 			type: "POST",
-			url: "/blog/api/user/login",
+			url: "/api/user/login",
 			contentType: "application/json; charset=UTF-8", // 요청 데이터의 MIME 타입
 			data: JSON.stringify(data), // 자바스크립트 객체 -> JSON 문자열, HTTP body
 			dataType: "json" // 응답 데이터의 타입, 기본적으로 모든 것은 문자열, JSON이면 자바스크립트 객체로 변경
 		}).done(function(resp) { // 응답 결과
 			alert("로그인 완료");
-			location.href = "/blog";
+			location.href = "/";
 		}).fail(function(error) {
 			alert(JSON.stringify(error));
 		});
