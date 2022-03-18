b0a071e388155f65708908a2dd22724554a5db37
changsup <changsup96@gmail.com>
2022-03-18 16:12:29 +0900

    Implement Spring Security login.

--- src/main/java/com/cos/blog/config/SecurityConfig.java
+++ src/main/java/com/cos/blog/config/SecurityConfig.java
@@ -1,12 +1,16 @@
 package com.cos.blog.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.cos.blog.config.auth.PrincipalDetailService;
 
 // Bean 등록: 스프링 컨테이너에서 객체를 관리할 수 있게 하는 것
 
@@ -15,22 +19,33 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableGlobalMethodSecurity(prePostEnabled = true) // 특정 주소로 접근을 하면 권한 및 인증을 미리 확인
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
+	@Autowired
+	private PrincipalDetailService principalDetailService;
+	
 	@Bean // Bean 등록 (IoC 관리)
 	public BCryptPasswordEncoder encodePWD() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	// 시큐리티가 대신 로그인, password를 가로채서 해당 password가 무엇으로 해시되어 회원가입이 되었는지 알아야
+	// 같은 해시로 암호화해서 DB에 있는 해시랑 비교 가능
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(principalDetailService).passwordEncoder(encodePWD());
 	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.csrf().disable() // csrf 토큰 비활성화 (테스트 시 사용)
-			.authorizeHttpRequests()
+			.authorizeRequests()
 				.antMatchers("/", "/auth/**" ,"/js/**", "/css/**", "/image/**") // 다음 요청은 누구나 접속 가능
 				.permitAll()
 				.anyRequest() // 다른 모든 요청 인증 필요
 				.authenticated()
 			.and()
 				.formLogin()
-				.loginPage("/auth/loginForm"); 
+				.loginPage("/auth/loginForm")
+				.loginProcessingUrl("/auth/loginProc") // 스프링 시큐리티가 해당 주소로 요청오는 로그인을 가로채서 대신 로그인
+				.defaultSuccessUrl("/");
 	}
 }
---
+++ src/main/java/com/cos/blog/config/auth/PrincipalDetail.java
@@ -0,0 +1,66 @@
+package com.cos.blog.config.auth;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.cos.blog.model.User;
+
+// 스프링 시큐리티가 로그인 요청을 가로채서 로그인을 진행하고 완료가 되면 UserDetails 타입의 객체를
+// 스프링 시큐리티의 고유한 세션저장소에 저장을 한다.
+public class PrincipalDetail implements UserDetails {
+	private User user; // 컴포지션
+	
+	
+	public PrincipalDetail(User user) {
+		this.user = user;
+	}
+
+	@Override
+	public String getPassword() {
+		return user.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		return user.getUsername();
+	}
+	
+	// 계정이 만료되지 않았는지 반환 (true: 만료 X)
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	// 계정이 잠겨있지 않았는지 반환 (true: 잠김 X)
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	// 비밀번호가 만료되지 않았는지 반환 (true: 만료 X)
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	// 계졍이 활성화(사용 가능)인지 반환 (true: 활성화)
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+	
+	// 계정이 갖고있는 권한 목록 반환 (권한이 여러 개 있을 수 있어서 루프를 돌아야 하는데 지금은 한 개만)
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		
+		Collection<GrantedAuthority> collectors = new ArrayList<>();
+		collectors.add(() -> {
+			return "ROLE_" + user.getRole();
+		});
+		
+		return collectors;
+	}
+}
