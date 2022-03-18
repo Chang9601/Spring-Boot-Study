f29356cf7ecd18bdf190b8763ad4be80be37b9be
changsup <changsup96@gmail.com>
2022-03-18 09:50:05 +0900

    Hash password.

--- src/main/java/com/cos/blog/config/SecurityConfig.java
+++ src/main/java/com/cos/blog/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.cos.blog.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 // Bean 등록: 스프링 컨테이너에서 객체를 관리할 수 있게 하는 것
 
@@ -12,11 +14,18 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity // 시큐리티 필터가 등록이 된다.
 @EnableGlobalMethodSecurity(prePostEnabled = true) // 특정 주소로 접근을 하면 권한 및 인증을 미리 확인
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Bean // Bean 등록 (IoC 관리)
+	public BCryptPasswordEncoder encodePWD() {
+		return new BCryptPasswordEncoder();
+	}
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
+			.csrf().disable() // csrf 토큰 비활성화 (테스트 시 사용)
 			.authorizeHttpRequests()
-				.antMatchers("/auth/**") // 다음 요청은 누구나 접속 가능
+				.antMatchers("/", "/auth/**" ,"/js/**", "/css/**", "/image/**") // 다음 요청은 누구나 접속 가능
 				.permitAll()
 				.anyRequest() // 다른 모든 요청 인증 필요
 				.authenticated()
--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -2,6 +2,7 @@ package com.cos.blog.controller.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,13 +21,7 @@ public class UserApiController {
 	@PostMapping("/auth/joinProc")
 	public ResponseDto<Integer> save(@RequestBody User user) {
 		System.out.println("UserApiController: save 호출");
-		// DB에 INSERT 후 아래 반환
-		user.setRole(RoleType.USER);
 		userService.회원가입(user);
-		
 		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1); // 자바 객체를 JSON으로 변환해서 반환(Jackson)
 	}
-	
-	
-	
 }
